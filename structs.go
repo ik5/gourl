5d8ffbc406deb06619f40465434ac086b8cba44b
Ido Kanner <idok@linesip.com>
2022-07-23 12:56:47 +0300

    basic structure for URL, prior of any other code

--- structs.go
+++ structs.go
@@ -6,24 +6,52 @@ type PathElement map[string][]string
 // QueryValue holds information regarding query arguments of key values, including repeated versions of them (based on RFCs).
 type QueryValue map[string][]string
 
-// URL holds information regarding the full components of address.
-type URL struct {
-	// Order of struct is based on memory alignment for better optimization.
+// CredentialsElement holds information regarding the username and password
+// provided by the URL.
+type CredentialsElement struct {
+	Username string
 
-	// Path elements of the URL
+	// Password is a clear text version of what was provided, please use with
+	// care.
+	Password string
+}
+
+// URL holds information regarding the full components of address.
+//
+// Important: Order of struct is based on memory alignment for better
+// optimization.
+type URL struct {
+
+	// Path elements of the URL.
 	Path PathElement
 
-	// Query elements based on
+	// Query elements based on.
 	Query QueryValue
+
+	// Credentials Holds information about user name and password.
+	Credentials CredentialsElement
 
 	// Scheme is the type of protocol component.
 	Scheme string
 
+	// Host name or IPv4/IPv6 element.
 	Host string
 
+	// Fragment holds the name of the fragment part
+	Fragment string
+
+	// ExtraInfo provides extra information based on specific protocol and it's
+	// parsing mechanism
+	ExtraInfo interface{}
+
+	// RawPath hold the original path provided to be parsed.
 	RawPath string
 
-	// WithAuthority is set to true if the protocol supports authority mechanism (username and password) such as HTTP, FTP and more.
+	// RawRequest holds the raw URL prior of parsing.
+	RawRequest string
+
+	// WithAuthority is set to true if the protocol supports authority
+	// mechanism (username and password) such as HTTP, FTP and more.
 	WithAuthority bool
 
 	// Port holds information regarding the port component.
